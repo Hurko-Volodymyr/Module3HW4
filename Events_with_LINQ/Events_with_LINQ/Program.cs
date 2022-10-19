c4a6e7bf49cbcc035cd3adf8f1886f9a93fd5809
Hurko Volodymyr <92858780+Hurko-Volodymyr@users.noreply.github.com>
2022-10-19 20:30:02 +0300

    Wrong try-catch version

--- Events_with_LINQ/Events_with_LINQ/Program.cs
+++ Events_with_LINQ/Events_with_LINQ/Program.cs
@@ -2,8 +2,31 @@
 {
     internal class Program
     {
+        public event Func<int, int, int>? DoSum;
+        public int Sum(int x, int y) => x + y;
+        public void TryCatchMethod(int x, int y)
+        {
+            try
+            {
+                Console.WriteLine(DoSum?.Invoke(x, y));
+            }
+            catch (Exception)
+            {
+                throw new Exception(nameof(DoSum));
+            }
+        }
+
         private static void Main(string[] args)
         {
+            var program = new Program();
+            var x = 1;
+            var y = 2;
+            var a = 3;
+            var b = 4;
+
+            program.DoSum = program.Sum;
+            program.DoSum += program.Sum;
+            program.TryCatchMethod(program.DoSum(x, y), program.DoSum(a, b));
         }
     }
 }
