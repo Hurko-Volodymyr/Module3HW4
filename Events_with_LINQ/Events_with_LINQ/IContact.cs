2b2a35e685a373cf21042ebdd72e782183c734c0
Hurko Volodymyr <92858780+Hurko-Volodymyr@users.noreply.github.com>
2022-10-20 21:05:38 +0300

    Add LINQ

---
+++ Events_with_LINQ/Events_with_LINQ/IContact.cs
@@ -0,0 +1,11 @@
+﻿namespace Delegates_with_LINQ.Core
+{
+    public interface IContact
+    {
+        public string? FirstName { get; set; }
+
+        public string? LastName { get; set; }
+
+        public long? Number { get; set; }
+    }
+}
--- Events_with_LINQ/Events_with_LINQ/Program.cs
+++ Events_with_LINQ/Events_with_LINQ/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Events_with_LINQ
+﻿using Delegates_with_LINQ.Core;
+
+namespace Events_with_LINQ
 {
     internal class Program
     {
@@ -27,6 +29,36 @@
             program.DoSum = program.Sum;
             program.DoSum += program.Sum;
             program.TryCatchMethod(program.DoSum(x, y), program.DoSum(a, b));
+
+            var collection = new List<Contact>
+            {
+                new Contact { FirstName = "Сулейман", LastName = "Фамилия2", Number = 380123123122 },
+                new Contact { FirstName = "89462-386=263", LastName = "7543747", Number = 387430123123122 },
+                new Contact { FirstName = "Зита", LastName = "Фамилия4", Number = 380123123124 },
+                new Contact { FirstName = "Sara", LastName = "Фамилия3", Number = 380123123123 },
+                new Contact { FirstName = "Abdula", LastName = "Фамилия1", Number = 380123123121 },
+                new Contact { FirstName = "gawegw", LastName = "awhr" }
+            };
+
+            foreach (var item in collection)
+            {
+                Console.WriteLine(item);
+            }
+
+            var sortedByDesNumberCollection = collection.OrderBy(x => x.Number).Reverse();
+            var firstOrDefaultItem = sortedByDesNumberCollection.Skip(3).FirstOrDefault();
+            var numberList = sortedByDesNumberCollection.Select(x => x.Number).Where(x => x.HasValue);
+
+            foreach (var item in sortedByDesNumberCollection)
+            {
+                Console.WriteLine($" Sorted: {item}");
+            }
+
+            Console.WriteLine($"          False first: {firstOrDefaultItem}");
+            foreach (var item in numberList)
+            {
+                Console.WriteLine($"               Number is: {item}");
+            }
         }
     }
 }
